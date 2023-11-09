80a3bc51cfaecaf69efc2fedc345b8dcc4b13525
Michael Harper <mharper@foreflight.com>
2023-11-09 10:50:16 -0800

    Initial Commit

---
+++ Local Notifications Watch App/Local_NotificationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Local_NotificationsApp.swift
+//  Local Notifications Watch App
+//
+//  Created by Michael Harper on 11/9/23.
+//
+
+import SwiftUI
+
+@main
+struct Local_Notifications_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Local Notifications/Local_NotificationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Local_NotificationsApp.swift
+//  Local Notifications
+//
+//  Created by Michael Harper on 11/9/23.
+//
+
+import SwiftUI
+
+@main
+struct Local_NotificationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
